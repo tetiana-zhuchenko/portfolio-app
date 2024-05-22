3276c6c97a75362d5d0167187211551b9f074fb2
Tania <taniyhamail@gmail.com>
2024-05-22 16:11:44 +0300

    fix: fixed page components for deployment

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,1 +1,5 @@
 import styles from './page.module.css'
+
+export const AboutMe = () => {
+  return <div>page</div>
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,1 +1,5 @@
 import styles from './page.module.css'
+
+export const Projects = () => {
+  return <div>page</div>
+}
